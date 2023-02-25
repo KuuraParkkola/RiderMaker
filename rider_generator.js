8950ef8a963b532ceb23539f49c1a1f9c3404725
Kuura <parkkola.kuura@gmail.com>
2023-02-25 15:11:13 +0200

    Add comments

--- rider_generator.js
+++ rider_generator.js
@@ -4,14 +4,17 @@ const { parse: yaml_parse } = require('yaml');
 
 
 const render = async () => {
+    /** Global settings */
     const settings = {
         social_icons: "./resources/social_icons",
         project_path: `./${process.argv[2]}`,
     }
 
+    /** Read a rider configuration from the given yaml file */
     const rawYaml = await fs.readFile(`${settings.project_path}/rider.yml`, { encoding: "utf-8" });
     const config = await yaml_parse(rawYaml);
 
+    /** Render a rider html file with EJS and save the html to the same folder where the configuration file resides */
     const renderedTemplate = await ejs.renderFile("./templates/template.ejs", { ...config, settings });
     await fs.writeFile(`${settings.project_path}/rider.html`, renderedTemplate);
 }
